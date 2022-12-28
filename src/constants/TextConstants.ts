875ec91403e8f9b35b82df09dce66037b218d74b
cthacker-udel <cthacker@udel.edu>
2022-12-27 21:22:40 -0500

    [master] Finished HomePage component

--- src/constants/TextConstants.ts
+++ src/constants/TextConstants.ts
@@ -2,6 +2,8 @@ export const TextConstants = {
     HOME_PAGE: {
         DESCRIPTION:
             "The Language Tracker Application. Is a creation of my habit, which is I would constantly practice programming problems on various websites, and practice every language I was familiar with, that includes languages such as Python, C, C++, C#, and Java to name a few. This is a daily routine, always making sure I at-least practice a problem once a day, or at the very least once in 2 days. This application seeks to organize that activity, and create a dashboard to house and track my practicing, and give me statistics or any sort of visualization of my journey from 0, to hero. I hope I can create an application that not only suits my purposes, but also yours. This application will not use the help of any other UI libraries, will be solely, and completely in-house made. Using React to pull data from the database, and manage states.",
+        LOGIN_BUTTON: "Login",
+        SIGN_UP_BUTTON: "Sign Up",
         TITLE: "Welcome to the Language Tracker Application!",
     },
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -15,5 +15,13 @@ export const HomePage = (): JSX.Element => (
         <div className={styles.home_page_content}>
             {TextConstants.HOME_PAGE.DESCRIPTION}
         </div>
+        <div className={styles.home_page_button_layout}>
+            <button className={styles.home_page_login_button} type="button">
+                {TextConstants.HOME_PAGE.LOGIN_BUTTON}
+            </button>
+            <button className={styles.home_page_sign_up_button} type="button">
+                {TextConstants.HOME_PAGE.SIGN_UP_BUTTON}
+            </button>
+        </div>
     </div>
 );
