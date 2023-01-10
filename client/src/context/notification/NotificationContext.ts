34ffe387221b86b60b0dde1ca26a00e253f2e929
cthacker-udel <cthacker@udel.edu>
2023-01-10 15:06:14 -0500

    [master] Removed notifications and changed login bg

--- client/src/context/notification/NotificationContext.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/indent -- disabled */
-
-import React from "react";
-
-import type { NotificationContextInterface } from "../../@types";
-
-export const NotificationContext = React.createContext<
-    NotificationContextInterface | undefined
->(undefined);
